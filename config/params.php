f9c0cca6f1d078d018dc90c927f6e5985d73bd5b
孙乔雨 <1589946526@qq.com>
2021-05-14 14:12:51 +0800

    修改params

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => '111@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 ];
